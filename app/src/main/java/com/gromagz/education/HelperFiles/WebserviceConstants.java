63442bf24b67345dd9d7b16f641e5aadf5ea8678
shaiks2004 <sau84746@gmail.com>
2025-06-09 23:43:44 +0530

    home screen

---
+++ app/src/main/java/com/gromagz/education/HelperFiles/WebserviceConstants.java
@@ -0,0 +1,22 @@
+package com.gromagz.education.HelperFiles;
+
+public class WebserviceConstants {
+
+    public static final int SUCCESS = 0;
+    public static final int FAILURE = -1;
+    public static final String KEY_STATUS_CODE = "responseStatus";
+    public static final String KEY_STATUS_MESSAGE = "responseMessage";
+    public static final String KEY_POSTS = "posts";
+
+    /**
+     * constants for all webservices
+     */
+    //this is url is used for all host
+    public static final String HTTP_BASE_URL = "https://";
+
+    public static String BASE_URL = "gromagz.in/";
+    public static String BASE_URL_TESTING = "gromagz.in/";
+
+
+
+}
